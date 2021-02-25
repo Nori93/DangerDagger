17e9f689aa12b422bef64ff77364b20e7fef6a21
Norbert <airdarkangel1@gmail.com>
2021-02-25 19:04:16 +0100

    death functions

---
+++ death_functions.py
@@ -0,0 +1,20 @@
+import tcod as libtcod
+from game_state import GameState
+from render_function import RenderOrder
+from game_message import Message
+
+def kill_player(player):  
+    player.color = libtcod.dark_red
+    return Message("You died!",libtcod.red), GameState.PLAYER_DEAD
+
+def kill_monster(monster):
+    death_message = Message("{} is dead!".format(monster.name.capitalize()), libtcod.orange)
+   
+    monster.color = libtcod.dark_red
+    monster.blocks = False
+    monster.fighter = None
+    monster.ai = None
+    monster.name = "remains of " + monster.name
+    monster.render_order = RenderOrder.CORPSE
+
+    return death_message
